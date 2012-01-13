f2b104b9bb486ccd59fe498be09530139e3030c9
Tom Christie <tom@tomchristie.com>
2012-01-13 13:52:14 +0000

    Added user.role

--- userroles/models.py
+++ userroles/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class UserRole(models.Model):
+    user = models.OneToOneField(User, related_name='role')
+    name = models.CharField(max_length=64)
+
+    def __eq__(self, other):
+        return unicode(self) == unicode(other)
+
+    def __unicode__(self):
+        return 'manager'
+
+
+#class UserProfile(models.Model):
+#    user = models.OneToOneField(User)
+#    role = models.ForeignKey(UserRole)
+
+
+def create_user_role(sender, instance, created, **kwargs):
+    if created:
+        UserRole.objects.create(user=instance)
+
+post_save.connect(create_user_role, sender=User)
--- userroles/tests.py
+++ userroles/tests.py
@@ -5,12 +5,15 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test.utils import override_settings
+
+from milkman.dairy import milkman
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    @override_settings(USER_ROLES=('manager', ''))
+    def test_creating_user_creates_role(self):
+        user = milkman.deliver(User)
+        self.assertEquals(user.role, 'manager')
