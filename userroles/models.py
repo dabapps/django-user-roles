66d53ecfcb2bf491beedbd6990d4e934bb00e98d
Tom Christie <tom@tomchristie.com>
2012-01-13 15:55:39 +0000

    Basic UserRoles

--- userroles/models.py
+++ userroles/models.py
@@ -1,26 +1,42 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
+
+import importlib
 
 
 class UserRole(models.Model):
     user = models.OneToOneField(User, related_name='role')
     name = models.CharField(max_length=64)
+    content_type = models.ForeignKey(ContentType, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     def __eq__(self, other):
         return unicode(self) == unicode(other)
 
     def __unicode__(self):
-        return 'manager'
+        return self.name
 
-
-#class UserProfile(models.Model):
-#    user = models.OneToOneField(User)
-#    role = models.ForeignKey(UserRole)
+    def __getattr__(self, name):
+        if name == '_content_object_cache':
+            return super(UserRole, self).__getattr__(name)
+        return getattr(self.content_object, name)
 
 
 def create_user_role(sender, instance, created, **kwargs):
     if created:
-        UserRole.objects.create(user=instance)
+        (name, user_role_str) = settings.USER_ROLES[0]
+        if user_role_str:
+            mod_str, cls_str = user_role_str.rsplit('.', 1)
+            user_role = getattr(importlib.import_module(mod_str), cls_str)
+            kwargs = {'content_object': user_role.objects.create()}
+        else:
+            kwargs = {}
+        UserRole.objects.create(user=instance, name=name, **kwargs)
+
 
 post_save.connect(create_user_role, sender=User)
--- userroles/tests.py
+++ userroles/tests.py
@@ -6,14 +6,38 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.contrib.auth.models import User
+from django.db import models
 from django.test import TestCase
 from django.test.utils import override_settings
 
 from milkman.dairy import milkman
+#from userroles.models import UserRole
+
+
+class ManagerRole(models.Model):
+    star_rating = models.IntegerField(default=3)
+
+
+class ClientRole(models.Model):
+    foobar = models.CharField(max_length=32)
+
+    #@property
+    #def custom_propery(self):
+    #    return True
 
 
 class SimpleTest(TestCase):
-    @override_settings(USER_ROLES=('manager', ''))
+    @override_settings(USER_ROLES=(('manager', ''),))
     def test_creating_user_creates_role(self):
         user = milkman.deliver(User)
         self.assertEquals(user.role, 'manager')
+
+    @override_settings(USER_ROLES=(('manager', 'userroles.tests.ManagerRole'),))
+    def test_custom_role_information(self):
+        user = milkman.deliver(User)
+        self.assertEquals(user.role.star_rating, 3)
+
+    #@override_settings(USER_ROLES=(('manager', 'userroles.tests.ManagerRole'),
+    #                                ('client', 'userroles.tests.ClientRole')))
+    #def test_create_user_role(self):
+    #    UserRole('client', foobar='baz')
