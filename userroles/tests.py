b6cd5762daeffe40fe2ba09a82bc75b0832a2134
Tom Christie <tom@tomchristie.com>
2012-01-16 10:21:06 +0000

    user_has_role decorator

--- userroles/tests.py
+++ userroles/tests.py
@@ -5,13 +5,25 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.conf.urls import patterns
 from django.contrib.auth.models import User
 from django.db import models
+from django.http import HttpResponse
 from django.test import TestCase
 from django.test.utils import override_settings
 
 from milkman.dairy import milkman
-#from userroles.models import UserRole
+
+from userroles.decorators import user_has_role
+
+urlpatterns = patterns('userroles.tests',
+    (r'^manager_only_view$', 'manager_only_view'),
+)
+
+
+@user_has_role('manager')
+def manager_only_view(request):
+    return HttpResponse('ok')
 
 
 class ManagerRole(models.Model):
@@ -21,10 +33,6 @@ class ManagerRole(models.Model):
 class ClientRole(models.Model):
     foobar = models.CharField(max_length=32)
 
-    #@property
-    #def custom_propery(self):
-    #    return True
-
 
 class SimpleTest(TestCase):
     @override_settings(USER_ROLES=(('manager', ''),))
@@ -33,11 +41,20 @@ class SimpleTest(TestCase):
         self.assertEquals(user.role, 'manager')
 
     @override_settings(USER_ROLES=(('manager', 'userroles.tests.ManagerRole'),))
-    def test_custom_role_information(self):
+    def test_get_custom_role_information(self):
         user = milkman.deliver(User)
         self.assertEquals(user.role.star_rating, 3)
+
+
+class ViewTests(TestCase):
+    urls = 'userroles.tests'
+
+    def test_get(self):
+        user = milkman.deliver(User)
+        #user.role = ManagerRole()
+        self.client.get('/manager_only_view')
 
     #@override_settings(USER_ROLES=(('manager', 'userroles.tests.ManagerRole'),
     #                                ('client', 'userroles.tests.ClientRole')))
     #def test_create_user_role(self):
-    #    UserRole('client', foobar='baz')
+    #    RoleProfile('client', foobar='baz')
