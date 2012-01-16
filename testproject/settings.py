e64381a8980405bab7acd43d5d2ed56cd5c6a854
Tom Christie <tom@tomchristie.com>
2012-01-16 09:28:16 +0000

    Settings typo.

--- testproject/settings.py
+++ testproject/settings.py
@@ -126,7 +126,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'userroles'
     'userroles',
 )
 
