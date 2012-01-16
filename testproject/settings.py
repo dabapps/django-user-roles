6108af19b86692ae3e4451c258564e9f3acfb001
Tom Christie <tom@tomchristie.com>
2012-01-16 09:26:37 +0000

    Fix database setting to separate directory

--- testproject/settings.py
+++ testproject/settings.py
@@ -16,6 +16,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_DIR, 'database/sqlite3.db'),   # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
