b777bda3ebba72ee24545f31e13768407bbfe05e
Tom Christie <tom@tomchristie.com>
2012-01-16 09:27:09 +0000

    Add userroles to INSTALLED_APPS

--- testproject/settings.py
+++ testproject/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'userroles'
+    'userroles',
 )
 
 # A sample logging configuration. The only tangible logging
