986d32b1a24e18ab7c858bafffac7fd473dd988c
Tom Christie <tom@tomchristie.com>
2012-01-26 12:06:55 +0000

    get `importlib` from `django.utils`, to support python 2.6

--- userroles/__init__.py
+++ userroles/__init__.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-import importlib
+from django.utils import importlib
 
 
 _IMPORT_FAILED = "Could not import role profile '%s'"
